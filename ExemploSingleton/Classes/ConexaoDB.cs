b1b0d2cc7178a1ac91113da559d8719ac648b1eb
Wellington Andrade <wellington.cruz@gestaosat.com>
2020-10-19 14:02:42 -0300

    Singleton

---
+++ ExemploSingleton/Classes/ConexaoDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExemploSingleton.Classes
+{
+    // Singleton 
+    public class ConexaoDB
+    {
+        private static ConexaoDB _instance;
+
+        // Esconde o construtor, dessa forma nao vai deixar instanciar 
+        protected ConexaoDB() { }
+
+        // Metodo que vai retornar singleton 
+        public static ConexaoDB Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new ConexaoDB();
+            }
+
+            return _instance;
+        }
+
+        public void Open()
+        {
+            Console.WriteLine("Abrindo conexao");
+        }
+    }
+}
---
+++ ExemploSingleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using ExemploSingleton.Classes;
+using System;
+
+namespace ExemploSingleton
+{
+    class Program
+    {
+        // Client 
+        static void Main()
+        {
+            var conexao = ConexaoDB.Instance();
+            conexao.Open();
+
+            var novaConexao = ConexaoDB.Instance();
+            novaConexao.Open();
+
+            Console.ReadKey();
+        }
+    }
+}
